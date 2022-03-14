3fb900772d20893864184194c34c830966efb067
kotesh_Mudila <69259490+kotesh-arya@users.noreply.github.com>
2022-03-14 23:59:14 +0530

    FEAT: Added toggle component using useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./styles.css";
-
+import { useState } from "react";
 export default function App() {
+  const changeBg = () => {
+    bgColor === "red" ? setBgColor("blue") : setBgColor("red");
+  };
+
+  const [bgColor, setBgColor] = useState("red");
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2 style={{ backgroundColor: bgColor }}>
+        start editing to see some magic happen!
+      </h2>
+      <button onClick={changeBg}>Toggle</button>
     </div>
   );
 }
