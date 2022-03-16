20ac39a16efd4eecec1aae3f0bd61840e0e52384
kotesh_Mudila <69259490+kotesh-arya@users.noreply.github.com>
2022-03-16 23:19:29 +0530

    FEAT: Added solutions upto EX-6

--- src/App.js
+++ src/App.js
@@ -23,10 +23,12 @@ export default function App() {
   //  Take two passwords from user. Show error only if both passwords don't match.
 
   const [validationText, setValidationText] = useState(
-    "Please Re-enter the exact password"
+    "Please confirm the password by entering it again"
   );
   const [firstInput, setFirstInput] = useState("");
   const [validationColor, setValidationColor] = useState("red");
+  const [visibility, setVisibility] = useState(false);
+
   const firstInputHandler = (e) => {
     setFirstInput(e.target.value);
   };
@@ -34,17 +36,21 @@ export default function App() {
     if (firstInput === e.target.value) {
       setValidationColor("green");
       setValidationText("Now you can reset the password");
+      setVisibility(false);
     } else {
       setValidationColor("red");
-      setValidationText("Please Re-enter the exact password");
-      style.display = "none";
+      setValidationText("Please re-enter the exact password");
+      setVisibility(true);
+    }
+    if (firstInput === null && e.target.value === null) {
+      setValidationText("Please enter valid Input");
     }
   };
-  const [numericValidationColor, setNumericValidationColor] = useState("black");
   //Password should contain a number. Show error if user misses character.
   const [numericInput, setNumericInput] = useState(
-    "Please enter strong password"
+    "Please enter a strong password"
   );
+  const [numericValidationColor, setNumericValidationColor] = useState("black");
 
   const numericChecker = (e) => {
     if (e.target.value.match(/\d/)) {
@@ -59,9 +65,75 @@ export default function App() {
   //   Add a submit button to password change field.
   // Disable submit button if passwords don't match
   // Clicking on submit should console the password field
+  const [secondInput, setSecondInput] = useState("");
+  const checkMatch = (e) => {
+    setSecondInput(e.target.value);
+    console.log(secondInput);
+  };
+  //////
+  const [buttonText, setButtonText] = useState("show password");
+  const [passwordType, setPasswordType] = useState("password");
+  const showPassword = () => {
+    if (passwordType === "password") {
+      setPasswordType("text");
+      setButtonText("Hide password");
+    } else {
+      setPasswordType("password");
+      setButtonText("show password");
+    }
+  };
 
+  //ex6: designer tool (mini figma) upto basic logic
+  const [pageFont, setPageFont] = useState(16);
+  const increaseSize = () => {
+    setPageFont(pageFont + 8);
+  };
+  const decreaseSize = () => {
+    setPageFont(pageFont - 8);
+  };
+  //   ex7: add to cart
+  // Show a list of items with add to cart button
+  // Clicking on the button should add that item to cart.
+  // Show the cart below with items added.
+  // Clicking on the same item twice should add more items in cart.
+  // CHALLENGE : increase the count of the item in cart without adding it to list.
+  const itemArr = [
+    { id: 0, title: "Fruits", done: true },
+    { id: 1, title: "Clothes", done: false },
+    { id: 2, title: "Gadgets", done: false }
+  ];
+  // const [itemCount, setItemsCount] = useState(0);
+
+  // const [addedItems, setAddedItems] = useState();
+  // const addItem = () => {
+  //   setItemsCount(itemCount + 1);
+  //   setAddedItems();
+  // };
   return (
-    <div className="App">
+    <div style={{ fontSize: pageFont }} className="App">
+      {/* <Card title='kotesh' age={32} color='red'/>
+      <Card title='kotesh' age={32}/>
+      <Card title='kotesh' age={32}/>
+      <Card title='kotesh' age={32}/>
+
+      <ul>
+        {itemArr.map((item) => {
+          return (
+            <li key={item.title}>
+              {item.title} <button onClick={addItem}>ADD TO CART</button> <br />{" "}
+              <br />
+            </li>
+          );
+        })}
+      </ul>
+      <hr />
+      <h5>Cart({itemCount})</h5>
+      <ul>
+        <li>{addedItems}</li>
+      </ul> */}
+      <button onClick={decreaseSize}>-</button> <br />
+      <button onClick={increaseSize}>+</button>
+      <h4>The current page font size is {pageFont}px </h4>
       <h2 style={{ backgroundColor: bgColor }}>
         start editing to see some magic happen!
       </h2>
@@ -76,11 +148,24 @@ export default function App() {
       <br /> <br />
       <input onChange={validaitonHandler} type="password" /> <br />
       <p style={{ color: validationColor }}>{validationText}</p>
-      <button>Reset Password</button> <br /> <br /> <hr />
+      <button onClick={checkMatch} disabled={visibility}>
+        SUBMIT
+      </button>{" "}
+      <br /> <br /> <hr />
       <br /> <br /> <br />
       <h2>Alpha-numeric Password checker</h2>
-      <input onChange={numericChecker} type="text" />
+      <input onChange={numericChecker} type={passwordType} />{" "}
+      <button onClick={showPassword}>{buttonText}</button>
       <p style={{ color: numericValidationColor }}>{numericInput}</p>
+    </div>
+  );
+}
+function Card({ title, age, color }) {
+  return (
+    <div>
+      <p>{age}</p>
+      <h1>{title}</h1>
+      <p>{color}</p>
     </div>
   );
 }
